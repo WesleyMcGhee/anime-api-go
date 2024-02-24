418fc2246811cad312030dd0881d7a96cdcb3366
Wesley McGhee <wesmcghee6372@gmail.com>
2024-02-23 20:50:49 -0600

    added route to api

--- routes/SFWRoutes.go
+++ routes/SFWRoutes.go
@@ -10,10 +10,11 @@ import (
 func SFWRoutes(param string) *http.Response {
 	routesParams := [8]string{"hug", "kiss", "slap", "punch", "wink", "pat", "kill", "cuddle"}
 	if slices.Contains(routesParams, param) == true {
+		// This honestly probably shouldn't be a panic
 		panic("not a valid param")
 	}
 
-	resp, err := http.Get("")
+	resp, err := http.Get("" + param)
 
 	// Probably shouldn't panic here and add some additional logging
 	if (err != nil) {
